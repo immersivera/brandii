82236b112289c140ea7faffc1b16aff9e752d5ea
Rashaan <rashaant03@gmail.com>
2025-06-01 11:39:52 -0500

    Updated LibraryPage.tsx

--- src/pages/LibraryPage.tsx
+++ src/pages/LibraryPage.tsx
@@ -105,7 +105,7 @@ export const LibraryPage: React.FC = () => {
                   onClick={() => navigate('/create')}
                   leftIcon={<Plus className="h-4 w-4" />}
                 >
-                  New Brand Kit
+                  New
                 </Button>
               </div>
             </div>
