1519e6278d28936ff4192c3bf2df1bb99af50662
Rashaan <rashaant03@gmail.com>
2025-06-01 13:16:09 -0500

    Updated Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -38,7 +38,7 @@ export const Header: React.FC = () => {
                 src="/logo.png" 
                 alt="Brandii" 
                 className={`h-8 w-auto ${
-                  theme === 'dark' ? 'invert-[.25]' : ''
+                  theme === 'dark' ? 'invert-[.50]' : ''
                 }`}
               />
             </Link>
