eb51f93bf69fc3cb91030d38837e7d2a6e3ebf43
Rashaan <rashaant03@gmail.com>
2025-06-01 15:33:51 -0500

    Updated DashboardPage.tsx

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -37,6 +37,7 @@ export const DashboardPage: React.FC = () => {
       const selectedAsset = brandKit.generated_assets.find(
         asset => asset.id === brandKit.logo.selected_asset_id
       );
+      console.log('logo selected', selectedAsset)
       if (selectedAsset?.image_data) return selectedAsset.image_data;
     }
 
