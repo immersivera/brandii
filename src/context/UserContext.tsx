9ed77c5bbd882c8d6f72cf479132e6ec0afd139f
Rashaan <rashaant03@gmail.com>
2025-06-03 23:33:44 -0500

    Updated UserContext.tsx

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -91,8 +91,7 @@ export const UserProvider: React.FC<{ children: React.ReactNode }> = ({ children
 
       if (session?.user) {
         setUserId(session.user.id);
-         const profiledata = fetchProfile(session.user.id);
-          console.log('profiledata', profiledata)
+         fetchProfile(session.user.id);
       } else {
         setUserId(null);
         setProfile(null);
